fb2d867aabacea6c0e7f5da9cb4bdd422903f34b
Lightwood13 <victor.kryshtal@gmail.com>
2022-08-01 12:53:22 +0300

    Initial commit

---
+++ src/main/kotlin/com/example/webscrapper/WebScrapperApplication.kt
@@ -0,0 +1,11 @@
+package com.example.webscrapper
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WebScrapperApplication
+
+fun main(args: Array<String>) {
+    runApplication<WebScrapperApplication>(*args)
+}
